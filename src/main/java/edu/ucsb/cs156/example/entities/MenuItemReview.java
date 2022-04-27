859c7cda8e446ff8130a009b3fa958b1a7559676
Sangita Kunapuli <sangita@169-231-122-213.wireless.ucsb.edu>
2022-04-27 10:42:01 -0700

    sk - updated entity menuitem class

---
+++ src/main/java/edu/ucsb/cs156/example/entities/MenuItemReview.java
@@ -0,0 +1,29 @@
+package edu.ucsb.cs156.example.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "menuitem")
+public class MenuItemReview {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long itemId;
+
+  private String reviewerEmail;
+  private int stars;  
+  private LocalDateTime dateReviewed;
+  private String comments;
+}
